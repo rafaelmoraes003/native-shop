086adab7d88e2bee9d2fd0759b7c574c868be2b8
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-06 16:21:18 -0300

    feat: cria interface NavigationProp

---
+++ src/interfaces/NavigationProp.ts
@@ -0,0 +1,6 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { TopNavigatorParamsList } from '../types/TopNavigatorParamsList';
+
+export interface NavigationProp {
+  navigation: NativeStackNavigationProp<TopNavigatorParamsList>
+}
