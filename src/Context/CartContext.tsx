35fc8f7e58be0d88fefb2314ea8be591dc77b017
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-05 23:57:48 -0300

    fix: lint errors

--- src/Context/CartContext.tsx
+++ src/Context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, SetStateAction } from 'react';
+import { createContext } from 'react';
 import { DefaultContextValueProps } from '../interfaces/DefaultContextValueProps';
 
 const defaultContextValue: DefaultContextValueProps = {
