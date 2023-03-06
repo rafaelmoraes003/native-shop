de7631fe80c66b3bf4fa5bbf2f55dadb4ecae5c0
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-06 20:11:29 -0300

    fix: dropdown icon color quando está habilitado ou desabilitado

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -9,11 +9,11 @@ export default function DropDown({ value, setValue, enabled }: DropDownProps) {
       onValueChange={(itemValue) => setValue(itemValue)}
       enabled={enabled}
       style={{ width: '90%' }}
-      dropdownIconColor={enabled ? '#808080' : '#7159C1'}
+      dropdownIconColor={enabled ? '#7159C1' : '#808080'}
     >
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((opt) => (
         <Picker.Item label={String(opt)} value={opt} key={opt} />
       ))}
     </Picker>
-  )
+  );
 }
