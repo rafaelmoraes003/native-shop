c9e0a0a8a35d22b5afe1d3d8bb1f5c38c125077c
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-05 23:31:50 -0300

    feat: implementa função loadLocalStorage, que retorna o que está no local storage

---
+++ src/utils/loadLocalStorage.ts
@@ -0,0 +1,14 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { IProduct } from '../interfaces/IProduct';
+
+const loadLocalStorage = async (): Promise<IProduct[]> => {
+  const dataFromLocalStorage: string | null = await AsyncStorage.getItem('cart');
+  if (!dataFromLocalStorage) {
+    await AsyncStorage.setItem('cart', JSON.stringify([]));
+    return [];
+  }
+  const cart = JSON.parse(dataFromLocalStorage as string) as IProduct[];
+  return cart;
+}
+
+export default loadLocalStorage;
