5fbc0e59aeb43dad0f58f4eff7edf22e67aac3eb
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-06 15:05:51 -0300

    feat: implementa provider no App

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Provider from './src/Context/Provider';
 import Products from './src/screens/Products';
 import Cart from './src/screens/Cart';
 
@@ -8,11 +9,13 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Products">
-        <Stack.Screen name="Products" component={Products} />
-        <Stack.Screen name="Cart" component={Cart} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Products">
+          <Stack.Screen name="Products" component={Products} />
+          <Stack.Screen name="Cart" component={Cart} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
