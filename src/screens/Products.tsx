4d72e1033428fbb37be1c0b5bd13ec676056966b
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-06 15:06:22 -0300

    feat: implementa useeffect para buscar produtos no banco de dados ao montar componente

--- src/screens/Products.tsx
+++ src/screens/Products.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from 'react-native';
+import getProductsFromFirestore from '../firebase/getProductsFromFirestore';
+import { IProduct } from '../interfaces/IProduct';
 
 export default function Products() {
-  return <Text>Products Screen</Text>;
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const getProducts = async (): Promise<void> => {
+      try {
+        const productsFromFirestore: IProduct[] = await getProductsFromFirestore();
+        setProducts(productsFromFirestore);
+      } catch (err) {
+        const error = err as Error;
+        console.log(error.message);
+      }
+    }
+    getProducts();
+  }, []);
+
+  return <Text>{JSON.stringify(products)}</Text>
 }
