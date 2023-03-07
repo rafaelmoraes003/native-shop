077c96203b429156d136827652b498eb8cb921fd
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-07 02:37:26 -0300

    feat: renderiza lista com itens do carrinho na tela do carrinho

--- src/screens/Cart.tsx
+++ src/screens/Cart.tsx
@@ -1,6 +1,32 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useContext } from 'react';
+import { Alert, FlatList, View } from 'react-native';
+import Header from '../components/Header';
+import CartContext from '../Context/CartContext';
+import CartItem from '../components/CartItem';
+import { NavigationProp } from '../interfaces/NavigationProp';
+import addToLocalStorage from '../utils/addToLocalStorage';
 
-export default function Cart() {
-  return <Text>Cart Screen</Text>;
+export default function Cart({ navigation }: NavigationProp) {
+  const { cartItems, setCartItems, setUpdate } = useContext(CartContext);
+
+  return (
+    <View>
+      <Header navigation={navigation} />
+      <View>
+        <FlatList
+          data={cartItems}
+          renderItem={({ item }) => (
+            <CartItem
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              price={item.price}
+              quantity={item.quantity}
+              imgUrl={item.imgUrl}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
 }
