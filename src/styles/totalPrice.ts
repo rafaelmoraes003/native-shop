b50d1741f8725bb942525ab428698eba4906a5c3
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-07 03:04:08 -0300

    style: totalPrice

---
+++ src/styles/totalPrice.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  text: {
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 16,
+    lineHeight: 19,
+    color: '#ffffff',
+  },
+  price: {
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: 'right',
+    letterSpacing: -1.6,
+    color: '#ffffff',
+  },
+});
+
+export default styles;
